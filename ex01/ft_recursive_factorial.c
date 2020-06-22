1e2068b984e79d0ee8eca116b18ba10d4f5ae19e
sbaloyi <sbaloyi@student.wethinkcode.co.za>
2020-06-22 09:52:59 +0200

    Initial ex01 commit

---
+++ ex01/ft_recursive_factorial.c
@@ -0,0 +1,10 @@
+int ft_recursive_factorial(int nb);
+
+int ft_recursive_factorial(int nb) 
+{ 
+	if (nb == 0) 
+		return (1);
+  	else if (nb < 0)
+      return (0);
+	return (nb * factorial(nb - 1)); 
+}
