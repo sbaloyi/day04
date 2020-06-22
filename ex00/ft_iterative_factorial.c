a76f4071685d6577659fe9d22c6ad27baf908f00
sbaloyi <sbaloyi@student.wethinkcode.co.za>
2020-06-22 09:48:43 +0200

    Updated ex00

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -2,9 +2,17 @@ int ft_iterative_factorial(int nb);
 
 int ft_iterative_factorial(int nb) 
 { 
-	if (nb == 0) 
-		return (1);
-  	else if (nb < 0)
-      return (0);
-	return (nb * factorial(nb - 1)); 
+	int sum = 1;
+
+    if (nb == 1) 
+		return (sum);
+	else if (nb < 0) 
+		return (0);
+
+    while (nb > 1)
+    {
+        sum *= nb;
+        nb--;
+    }
+    return (sum);
 }
