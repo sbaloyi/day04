45757c9221e7bd4a187ebc0ab4800350d5fbc98e
sbaloyi <sbaloyi@student.wethinkcode.co.za>
2020-06-23 08:56:52 +0200

    Initial ex05 commit

---
+++ ex05/ft_sqrt.c
@@ -0,0 +1,13 @@
+int     ft_sqrt(int nb);
+
+int		ft_sqrt(int nb)
+{
+    int counter = 0;
+    while (counter < nb)
+    {
+        if (counter * counter == nb)
+            return (counter);
+        counter++;
+    }
+    return (0);
+}
