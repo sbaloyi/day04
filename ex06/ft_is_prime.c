dc8bfa0c8a9558e4e283ec6c886dbf380c3d45dd
sbaloyi <sbaloyi@student.wethinkcode.co.za>
2020-06-23 09:53:08 +0200

    Initial ex07 commit

---
+++ ex06/ft_is_prime.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int ft_is_prime(int nb);
+
+int ft_is_prime(int nb) 
+{
+	int i = 2;
+	int counter = 0;
+	while (i <= nb / 2) 
+	{
+		if (nb % i == 0)
+		{
+			counter++;
+			break;
+		}
+		i++;
+	}
+	if (counter == 0 && nb != 1)
+		return (1);
+	else 
+		return (0);
+}
