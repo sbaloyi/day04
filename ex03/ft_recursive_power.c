6af66014d1eab1d050e6a4b46bfcecbcb853542e
sbaloyi <66995239+sbaloyi@users.noreply.github.com>
2020-06-25 10:11:12 +0200

    Updated return type for method

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -1,6 +1,6 @@
-void ft_recursive_power(int nb, int power);
+int ft_recursive_power(int nb, int power);
 
-void ft_recursive_power(int nb, int power) 
+int ft_recursive_power(int nb, int power) 
 {
 	if (power == 0) 
 		return (1);
